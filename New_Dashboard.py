c6c45038f1771654f836c2a6fe7f61265de9b902
KevinAndani <andani.kevin1610@gmail.com>
2024-07-10 13:15:00 +0530

    add New_dashboard to code base

---
+++ New_Dashboard.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from utils.pageconfig import page_config
+from utils.anedya import anedya_config, fetchHumidityData, fetchTemperatureData, GetFanStatus, GetHumidifierStatus
+from utils.global_vars import nodeId, apiKey, humidityData, temperatureData
+from utils.action_buttons import ButtonText, State, CurrentData
+from utils.login import drawLogin
+from utils.dashboard import drawDashboard
+
+
+page_config()
+
+def main():
+    global nodeId, apiKey
+    global humidityData, temperatureData
+    anedya_config(NODE_ID=nodeId,API_KEY=apiKey)
+
+    if"LoggedIn" not in st.session_state:
+        st.session_state.LoggedIn = False
+
+    ButtonText()
+    State()
+    CurrentData()
+
+    if st.session_state.LoggedIn is False:
+        drawLogin()
+    else:
+        humidityData = fetchHumidityData()
+        temperatureData = fetchTemperatureData()
+
+        GetFanStatus()
+        GetHumidifierStatus()
+
+        drawDashboard()
+
+if __name__ == "__main__":
+    main()
