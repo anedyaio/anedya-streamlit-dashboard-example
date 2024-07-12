b1ad169fa7fb61d91852fddba7627112232ec6ba
KevinAndani <168438399+KevinAndani@users.noreply.github.com>
2024-07-13 02:35:45 +0530

    Update global_vars.py

--- utils/global_vars.py
+++ utils/global_vars.py
@@ -2,8 +2,8 @@
 username = ""
 password = ""
 
-nodeId = "300e6de2-25ab-11ef-84ba-139754672fc5"  # get it from anedya dashboard -> project -> node 
-apiKey = "de86d4be9edd1229709dc8de0c3376a5b92586ed6c394db93cc12cadeee3077a"
+nodeId = "Write your node ID"  # get it from anedya dashboard -> project -> node 
+apiKey = "Write your api key"
 
 multiselect = []
 select = []
